af4dfd5ddb8af7540a93bfb4ce196473fa78b44b
Hoang Tran <hoangtlm2410@gmail.com>
2022-08-01 17:24:05 +0700

    build categories slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,64 @@
 // Render Quick link in Header-bottom
 const quickLinkList = document.querySelector('.header__bottom-quicklink--list')
-const quickLinkItems = [
-    {
-        id: 1,
-        title: 'trái cây'
-    },
-    {
-        id: 2,
-        title: 'thịt, trứng'
-    },
-    {
-        id: 3,
-        title: 'rau củ quả'
-    },
-    {
-        id: 4,
-        title: 'sữa, bơ, phô mai'
-    },
-    {
-        id: 5,
-        title: 'hải sản'
-    },
-    {
-        id: 6,
-        title: 'gạo, mì ăn liền'
-    },
-    {
-        id: 7,
-        title: 'đồ uống, bia rượu'
-    },
-    {
-        id: 8,
-        title: 'bánh kẹo'
-    },
-]
+const quickLinkItems = ['trái cây', 'thịt, trứng', 'rau củ quả', 'sữa, bỡ, phô mai', 
+                        'hải sản', 'gạo, mì ăn liền', 'đồ uống, bia rượụ', 'bánh kẹo']
 
 const renderQuickLinkItems = () => {
     quickLinkList.innerHTML += quickLinkItems.map(item => `
         <li class="header__bottom-quicklink-item">
-            <a href="/" class="header__bottom-quicklink-link">${item.title}</a>
+            <a href="/" class="header__bottom-quicklink-link">${item}</a>
         </li>
     `).join('')
 }
 
 renderQuickLinkItems()
+
+//Handle Slider Categories
+const prevIcon = document.querySelector('.content__categories-icon--wrapper.icon-prev')
+const nextIcon = document.querySelector('.content__categories-icon--wrapper.icon-next')
+const categoriesItems = document.querySelectorAll('.content__categories-item')
+
+const renderDefaultCategoriesList = () => {
+    categoriesItems.forEach((item, index) => {
+        if(index >= 13){
+            item.classList.add('d-none')
+        }
+    })
+}
+
+renderDefaultCategoriesList()
+
+const handleDisplayListItem = () => {
+    nextIcon.onclick = () => {
+        categoriesItems.forEach((item, index) => {
+            if(index < 13){
+                item.classList.add('d-none')
+                nextIcon.classList.add('d-none')
+            }else{
+                item.classList.remove('d-none')
+                // item.style = `transform: translateX(-1240px); transition: all 0.5s ease-in-out 0s;`
+                item.parentElement.parentElement.classList.remove('justify-content-between')
+                prevIcon.classList.remove('d-none')
+            }
+        })
+    }
+
+    prevIcon.onclick = () => {
+        categoriesItems.forEach((item, index) => {
+            if(index >= 13){
+                item.style.transform = 'translateX: -1240px;'
+                item.classList.add('d-none')
+                prevIcon.classList.add('d-none')
+            }else{
+                item.classList.remove('d-none')
+                item.parentElement.parentElement.classList.add('justify-content-between')
+                nextIcon.classList.remove('d-none')
+            }
+        })
+    }
+}
+
+handleDisplayListItem()
+
+
+
