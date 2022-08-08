034b0b24556605e355a1c898009423706db036fa
Le Minh Hoang Tran <82258999+tranleminhhoang2410@users.noreply.github.com>
2022-08-09 00:28:16 +0700

    build ui featured categories

--- assets/js/content/authenticBrand.js
+++ assets/js/content/authenticBrand.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
         nextArrow: '<a href="/" class="content__authentic-brand--slider__btn next-btn d-flex flex-row-reverse align-items-center position-absolute top-0"><img src="./assets/images/global/arrows/rectangleArrow.png"></a>',
         dots: true,
         dotsClass: 'content__authentic-brand__dot-list d-flex justify-content-center position-absolute'
-    });
+    }); 
 
     //Change list product
     $('.content__authentic-brand--product__list').slick({  
